61de422403abc2437bfa2dbdc99cda2ff445a17c
Hojin Choe <hojin.choe.dev@gmail.com>
2018-11-03 15:45:04 +0900

    feat: Add modules to get image binary data

--- libs/HttpRequest.js
+++ libs/HttpRequest.js
@@ -66,8 +66,70 @@ var connect = function (url, options, callback) {
     }, options.requestDelay);
 };
 
+var imageConnect = function (url, options, callback) {
+    options = options || {};
+
+    var defaultOptions = {
+        method: "GET",
+        charset: "UTF-8",
+        params: {},
+        headers: {},
+        requestDelay: 100,
+        requestTimeout: 60000
+    };
+
+    var keys = Object.keys(defaultOptions);
+
+    for (var i = 0; i < keys.length; i++)
+        options[keys[i]] = (typeof options[keys[i]] === "undefined") ? defaultOptions[keys[i]] : options[keys[i]];
+
+    var connectInfo = {
+        url: url,
+        method: options.method,
+        encoding: "binary",
+        timeout: options.requestTimeout,
+        headers: options.headers
+    };
+
+    if (options.method == "GET")
+        connectInfo.qs = options.params;
+    else if (options.method == "POST")
+        connectInfo.form = options.params;
+
+    setTimeout(function () {
+        request(connectInfo, function (error, response, html) {
+            if (error) {
+                console.log("HttpRequest.request() : " + error.code);
+                console.log("Connection Information : ");
+                console.log(connectInfo);
+                callback(null, null, error);
+                return;
+            }
+
+            if (response.statusCode != 200) {
+                console.log("HttpRequest.request() : Response Status Code : " + response.statusCode);
+
+                error = {
+                    status: false,
+                    contents: "HttpRequest.request() : Response Status Code is NOT 200"
+                };
+
+                callback(null, null, error);
+                return;
+            }
+
+            if (!error && response.statusCode == 200) {
+                var imgBuf = new Buffer(html, "binary");
+                callback(imgBuf, error);
+                return;
+            }
+        });
+    }, options.requestDelay);
+};
+
 var obj = {
-    connect: connect
+    connect: connect,
+    imageConnect: imageConnect
 };
 
 exports = module.exports = obj;
