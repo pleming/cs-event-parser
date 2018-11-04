2e2874972b992ad701732c30055e6be57cbbd078
Hojin Choe <hojin.choe.dev@gmail.com>
2018-11-04 10:01:16 +0900

    refactor: Modify modules to get CU goods image

--- parser/cu_parser.js
+++ parser/cu_parser.js
@@ -1,5 +1,4 @@
 var http = require("../libs/HttpRequest");
-var request = require("request");
 
 var eventTypeInfo = [
     {
@@ -22,17 +21,10 @@ var collectImage = function (productInfo, productIdx, callback) {
         return;
     }
 
-    request({
-        url: productInfo[productIdx].imgUrl,
-        encoding: "binary",
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-        }
-    }, function (err, res, body) {
+    http.imageConnect(productInfo[productIdx].imgUrl, {}, function(imgBuf, error) {
         console.log((productIdx + 1) + " : " + productInfo[productIdx].name + "(" + productInfo[productIdx].imgUrl + ") Complete.");
 
-        var imgByte = new Buffer(body, "binary");
-        productInfo[productIdx].image = imgByte;
+        productInfo[productIdx].image = imgBuf;
         delete productInfo[productIdx].imgUrl;
 
         collectImage(productInfo, productIdx + 1, callback);
