d8ffd420d9d46ca068d5762c6360f39e652e12ee
Hojin Choe <hojin.choe.dev@gmail.com>
2018-11-06 14:33:50 +0900

    feat: Add modules to get 7-ELEVEN goods image

--- parser/seveneleven_parser.js
+++ parser/seveneleven_parser.js
@@ -2,13 +2,23 @@ var http = require("../libs/HttpRequest");
 
 var eventTypeInfo = [1, 2];
 
-var collectProductInfo = function (productInfo, eventType, intCurrPage, callback) {
-    console.log(JSON.stringify({
-        intPageSize: 10,
-        intCurrPage: intCurrPage,
-        pTab: eventType
-    }));
+var collectImage = function (productInfo, productIdx, callback) {
+    if (productIdx == productInfo.length) {
+        callback();
+        return;
+    }
 
+    http.imageConnect(productInfo[productIdx].imgUrl, {}, function (imgBuf, error) {
+        console.log((productIdx + 1) + " : " + productInfo[productIdx].name + "(" + productInfo[productIdx].imgUrl + ") Complete.");
+
+        productInfo[productIdx].image = imgBuf;
+        delete productInfo[productIdx].imgUrl;
+
+        collectImage(productInfo, productIdx + 1, callback);
+    });
+};
+
+var collectProductInfo = function (productInfo, eventType, intCurrPage, callback) {
     http.connect("http://www.7-eleven.co.kr/product/listMoreAjax.asp", {
         method: "POST",
         params: {
@@ -26,11 +36,14 @@ var collectProductInfo = function (productInfo, eventType, intCurrPage, callback
             var prodObj = {
                 name: $(elem).find("div.name").text(),
                 price: parseInt($(elem).find("div.price").text().replace(",", "")),
-                eventType: eventType
+                eventType: eventType,
+                imgUrl: "http://www.7-eleven.co.kr" + $(elem).find("img").attr("src")
             };
 
             productInfo.push(prodObj);
             passFlag = true;
+
+            console.log(productInfo.length + " : " + prodObj.name + " Complete.");
         });
 
         if (passFlag == false) {
@@ -57,9 +70,10 @@ var run = function (callback) {
     var productInfo = [];
 
     collectEventType(productInfo, 0, function () {
-        callback(productInfo);
+        collectImage(productInfo, 0, function () {
+            callback(productInfo);
+        });
     });
-
 };
 
 var obj = {
